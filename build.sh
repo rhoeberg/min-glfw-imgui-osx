296f0e603f8c56af1c5463b67999d9098d13436c
rhoeberg <contact@rhoeberg.com>
2019-07-12 17:10:55 +0200

    minimum glfw example for osx

---
+++ build.sh
@@ -0,0 +1,8 @@
+g++ main.cpp \
+-framework opengl \
+-lGLEW \
+-lglfw3 \
+-framework IOKit \
+-framework CoreVideo \
+-framework Cocoa \
+-o test
---
+++ main.cpp
@@ -0,0 +1,239 @@
+#define GLEW_STATIC
+#include "GL/glew.h"
+#include <GLFW/glfw3.h>
+
+// GLM Mathematics
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+#include <iostream>
+
+const int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 600;
+bool keys[1024];
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    	glfwSetWindowShouldClose(window, GL_TRUE);
+
+    if(action == GLFW_PRESS) {
+        keys[key] = true;
+    }
+    else if(action == GLFW_RELEASE) {
+        keys[key] = false;
+    }
+}  
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    // mouseX = xpos;
+    // mouseY = ypos;
+}
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+char* readFile(const char* fileName)
+{
+    char* result = 0;
+    FILE* file = fopen(fileName, "r");
+    if(file) {
+        fseek(file, 0, SEEK_END);
+        size_t fileSize = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        
+        result = (char*)malloc(fileSize + 1);
+        fread(result, fileSize, 1, file);
+        result[fileSize] = 0;
+
+        fclose(file);
+    }
+    else {
+        printf("cannot find file %s\n", fileName);
+    }
+
+    return result;
+}
+
+GLuint create_shader(const char *vPath, const char *fPath)
+{
+    GLuint shaderProgram;
+    const char* vertexCode = readFile(vPath);
+    const char* fragmentCode = readFile(fPath);
+
+    GLint success;
+    GLchar infoLog[512];
+    GLuint vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexCode, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+	glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+	printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
+        printf("%s\n", infoLog);
+    }
+    GLuint fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentCode, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+	glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+	printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
+        printf("%s\n", infoLog);
+    }
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+	glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+void init_glfw(GLFWwindow **win)
+{
+    glfwSetErrorCallback(error_callback);
+    if(!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    *win = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "great concept", NULL, NULL);
+    if(!win) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(*win);
+    
+    glfwSetCursorPosCallback(*win, mouse_callback);
+    glfwSetKeyCallback(*win, key_callback);
+
+    // Set this to true so glew knows to use modern opengl
+    glewExperimental = GL_TRUE;
+    glewInit();
+    
+    int vWidth, vHeight;
+    glfwGetFramebufferSize(*win, &vWidth, &vHeight);
+    glViewport(0, 0, vWidth, vHeight);
+    
+    // enable depths testing to remove pixels which is behind other pixels
+    glEnable(GL_DEPTH_TEST);  
+}
+
+GLuint createCube()
+{
+    const GLfloat vertices[] = {
+       -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+       -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        
+       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+       -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+       -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+       -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+       -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+       -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    
+    return VAO;
+}
+
+int main(int argc, char *argv[])
+{
+    GLFWwindow *win;
+    init_glfw(&win);
+    
+    GLuint shaderProgram = create_shader("vertexshader.vs", "fragmentshader.frag");
+    GLuint cube = createCube();
+    
+    while(!glfwWindowShouldClose(win)) {
+        glfwPollEvents();
+        glClearColor(0.2f, 0.6f, 0.8f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  
+        
+        glUseProgram(shaderProgram);
+        glm::mat4 view;
+        glm::mat4 projection;
+        glm::mat4 model;
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 1000.0f);
+        model = glm::translate(model, glm::vec3(0,0,0));
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0,1,0));
+        GLuint viewLoc = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        GLuint projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        GLuint modelLoc = glGetUniformLocation(shaderProgram, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        glBindVertexArray(cube);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(win);
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
